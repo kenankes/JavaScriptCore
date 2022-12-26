c6a7a8656bdf0b1b260e1cca1d6a268b87b8aaed
KESTANECI Kenan <kenan.kestaneci@soprasteria.com>
2022-12-26 16:11:55 +0100

    JavaScriptBasics initialized

--- script.js
+++
@@ -1,7 +0,0 @@
-alert("Hello World!");
-
-//Using the showMessage function
-showMessage("PROMOTION !!");
-
-//To print something in console.
-console.log("Hello World");
--- utils.js
+++
@@ -1,4 +0,0 @@
-//A function to modify text content by ID in the webpage.
-function showMessage(message) {
-    document.getElementById('message').textContent = message;
-}
